caac5597cc0f26b94f56066f424c90f4bbaa8c79
vcho1958 <gusckd57@gmail.com>
2023-02-01 08:02:18 +0900

    init

---
+++ src/test/java/com/example/ecommerce/userservice/EcommerceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
