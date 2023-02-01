dc34cf83589879fe841234409724d2678b820c3d
vcho1958 <gusckd57@gmail.com>
2023-02-01 09:13:10 +0900

    setting: register to eureka

--- src/main/java/com/example/ecommerce/userservice/EcommerceUserApplication.java
+++ src/main/java/com/example/ecommerce/userservice/EcommerceUserApplication.java
@@ -1,9 +1,12 @@
 package com.example.ecommerce.userservice;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EcommerceUserApplication {
 
     public static void main(String[] args) {
